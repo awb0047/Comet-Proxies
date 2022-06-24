0106264cbf2c622cbf174a45529f6f0181b8143b
Splooti <add9mob@gmail.com>
2022-06-23 19:08:33 -0500

    Moved API calls to server instead of local host

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
 export function getUserDetails() {
-    return axios.get('http://localhost:3001/api/auth', {
+    return axios.get('http://45.33.101.29:3001/api/auth', {
         withCredentials: true } );
 }
 
 export function getUserData(proxyUser) {
-    return axios.get(`http://localhost:3001/api/users/data/${proxyUser}`, {
+    return axios.get(`http://45.33.101.29:3001/api/users/data/${proxyUser}`, {
         withCredentials: true } );
 }
 
 export function getUserPlans(proxyUser) {
-    return axios.get(`http://localhost:3001/api/users/plans/${proxyUser}`, {
+    return axios.get(`http://45.33.101.29:3001/api/users/plans/${proxyUser}`, {
         withCredentials: true } );
 }
 
 export function genProxies(proxyUser, proxyPass, sid, bandwidth) {
     var proxyList = [];
-    axios.post('http://localhost:3001/api/gen/proxies', {
+    axios.post('http://45.33.101.29:3001/api/gen/proxies', {
         proxyUser: proxyUser,
         proxyPass: proxyPass,
         sid: sid,
@@ -46,7 +46,7 @@ export async function stripeCheckout(id, discordId, discordTag, username, passwo
         } )
     }
 
-    const response = await fetch('http://localhost:3001/api/stripe/checkout', options)
+    const response = await fetch('http://45.33.101.29:3001/api/stripe/checkout', options)
     const result = await response.json();
     console.log(result.url);
     window.location = result.url;
