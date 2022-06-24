0dca225c1c53e6542dff9a71023031a93226a605
Splooti <add9mob@gmail.com>
2022-06-24 16:04:20 -0500

    test

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const subdomain = 'https://www.cometproxies.com/'
 
 export function login() {
-    window.location.href = `api/auth/discord`
+    window.location.href = `auth/discord`
 }
 
 export function logout() {
