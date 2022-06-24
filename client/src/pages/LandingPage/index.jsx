68feb8995a1cde8b88fe7aa04661395a052847af
Splooti <add9mob@gmail.com>
2022-06-23 23:25:25 -0500

    .

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from '@chakra-ui/react'
 
 export function LandingPage( props ) {
-    const login = () => window.location.href = 'https://api.cometproxies.com/api/auth/discord';
+    const login = () => window.location.href = 'http://45.33.101.29/api/auth/discord';
     return (
         <Button
             onClick={login}
