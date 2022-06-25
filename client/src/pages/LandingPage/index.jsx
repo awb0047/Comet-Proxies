9c99a071ad6ad766302c53002b5115856fd7144a
Splooti <add9mob@gmail.com>
2022-06-25 12:19:58 -0500

    Landing Page Improvement

--- client/src/pages/LandingPage/index.jsx
+++ client/src/pages/LandingPage/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import "../../utils/styles/landingstyle.css"
 import { Button } from '@chakra-ui/react'
 import { login } from '../../utils/api'
 
 export function LandingPage( props ) {
     return (
-        <Button
+        <div className="landing-container">
+            <img src='https://media.discordapp.net/attachments/974850038381547521/986083447737044992/logo_transparent.png' width='300px'/>
+            <Button
             onClick={login}
             colorScheme='blue'
-        >Login</Button>
+            mt='30px'
+            >Login With Discord</Button>
+        </div>
     )
 }
