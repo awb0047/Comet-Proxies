ad1e47ba74bc69a58bbc80b9cd2d75402bf0e6cc
Splooti <add9mob@gmail.com>
2022-07-13 16:47:53 -0500

    Test

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ const subdomain = 'https://www.cometproxies.com/'
 export async function login() {
     try {
         const response = await fetch(`${subdomain}api/auth/`);
-        console.log(response.status);
+        console.log("status here");
         window.location.href = `${subdomain}api/auth/discord/redirect`
     } catch (err) {
         window.location.href = `https://www.cometproxies.com/down`
