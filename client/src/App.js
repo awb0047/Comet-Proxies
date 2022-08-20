eaf777ff4450903b71db3a53681d7e904086f1fb
awb0047 <add9mob@gmail.com>
2022-08-20 16:10:45 -0500

    pathing update

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,11 @@ function App() {
   return (
     <div class='AppContainer'>
         <Routes>
-          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/dashboard/login' element={<LoginPage/>} />
           <Route path='/menu' element={<MenuPage/>} />
           <Route path='/dashboard' element={<DashboardPage/>} />
-          <Route path='/down' element={<DownPage/>} />
-          <Route path='/terms' element={<TermsPage/>} />
+          <Route path='/dashboard/down' element={<DownPage/>} />
+          <Route path='/dashboard/terms' element={<TermsPage/>} />
         </Routes>
     </div>
   );
